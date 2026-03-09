31ac03c4c14305909d28dd91cb80f91727c210f2
MichelAngelo <baksdevelopmen@gmail.com>
2026-03-10 02:06:45 +0300

    BaksDevelopment

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -29,8 +29,7 @@ return static function(ContainerConfigurator $configurator) {
     $services = $configurator->services()
         ->defaults()
         ->autowire()
-        ->autoconfigure()
-        ->public();
+        ->autoconfigure(); //->public();
 
     $NAMESPACE = BaksDevFilesResourcesBundle::NAMESPACE;
     $PATH = BaksDevFilesResourcesBundle::PATH;
